954bc1bfebda7cee0e272ee25d574231c0020869
justTrueCodeWriter <dmjustcoder8@gmail.com>
2023-12-27 13:39:12 +0300

    upd

--- src/Hero.cpp
+++ src/Hero.cpp
@@ -58,15 +58,18 @@ void Hero::move(float time) {
       sendMessage("attack");
       speed = dash(time, isConsistent);
     }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && (message_.find("collide")==std::string::npos)) {
-      y_ -= 0.9*time;
-
-      current_frame += 0.01*time;
-      if (current_frame > 4) current_frame = 0;
-      if (side_ > 0)
-        sprite.setTextureRect(sf::IntRect(39+(int(current_frame)*32), 650, 32, 44));
-      else if  (side_ < 0)
-        sprite.setTextureRect(sf::IntRect(39+(int(current_frame)*32)+32, 650, -32, 44));
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
+      if (message_.find("collide")!=std::string::npos)
+        y_ -= 0.5 * time;
+      if (message_.find("collide")==std::string::npos) {
+        y_ -= 0.9*time;
+        current_frame += 0.01*time;
+        if (current_frame > 4) current_frame = 0;
+        if (side_ > 0)
+          sprite.setTextureRect(sf::IntRect(39+(int(current_frame)*32), 650, 32, 44));
+        else if  (side_ < 0)
+          sprite.setTextureRect(sf::IntRect(39+(int(current_frame)*32)+32, 650, -32, 44));
+      }
     }
     else {
       current_frame += 0.01*time;
@@ -87,7 +90,7 @@ void Hero::move(float time) {
     }
 
     x_ += side_*speed*time;
-    if ((message_.find("collide")==std::string::npos) && !isConsistent && !sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    if ((message_.find("collide")==std::string::npos) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
       y_ += 0.9*time;
     sprite.setPosition(x_, y_); 
     message_.clear();
